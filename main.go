bd7e024c172e6a6328049650f7ee360134cfd8f1
gido5731 <gido5731@gido-desk-deb-v2.imagen.click>
2020-03-17 17:07:51 -0400

    download the alexa list and load into memory

---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"archive/zip"
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/gidoBOSSftw5731/log"
+)
+
+const (
+	domListZip = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
+)
+
+func main() {
+	log.SetCallDepth(4)
+	tmp := os.TempDir()
+	zipPath := path.Join(tmp, "domList.zip")
+
+	err := downloadFile(zipPath, domListZip)
+	if err != nil {
+		log.Fatalln("Couldn't download file! ", err)
+	}
+
+	csvArr, err := unzip(zipPath, tmp)
+
+	if len(csvArr) != 1 {
+		log.Fatalln("Error! multiple files extracted!")
+	} else if err != nil {
+		log.Fatalln("Error extracting! ", err)
+	}
+
+	// csv file downloaded and extracted
+
+	var entries []string
+	csvFile, _ := os.Open(csvArr[0])
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	// yes, I know it's single-threaded, but it's fast enough that I dont care
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		entries = append(entries, line[1])
+	}
+
+	
+}
+
+// downloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func downloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	log.Traceln("Downloaded")
+	return err
+}
+
+// unzip will decompress a zip archive, moving all files and folders
+// within the zip file (parameter 1) to an output directory (parameter 2).
+func unzip(src string, dest string) ([]string, error) {
+
+	var filenames []string
+
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return filenames, err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+
+		// Store filename/path for returning and using later on
+		fpath := filepath.Join(dest, f.Name)
+
+		// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+		if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s: illegal file path", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+
+		if f.FileInfo().IsDir() {
+			// Make Folder
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		// Make File
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		// Close the file without defer to close before next iteration of loop
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
+}
